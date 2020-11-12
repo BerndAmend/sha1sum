530234fc36cf881d6e146e5011b1dcc81132c258
Bernd Amend <bernd.amend@gmail.com>
2020-11-12 09:49:26 +0100

    remove "using namespace std;"

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 #include "SHA1Sum.h"
 #include <iostream>
 
-using namespace std;
-
 int main() {
   if (SHA1Sum("The quick brown fox jumps over the lazy dog").str() !=
       "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12") {
@@ -23,7 +21,7 @@ int main() {
   }
 
   if (SHA1Sum("").str() != "da39a3ee5e6b4b0d3255bfef95601890afd80709") {
-    cout << "fail\n";
+    std::cout << "fail\n";
     return -3;
   }
 
